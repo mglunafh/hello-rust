e7172e3c042cb51c6213c52fe02f0e9670010af2
Fedor Uvarychev <ufispb@gmail.com>
2020-05-27 09:25:56 +0300

    Senums: aliases, use in local scope, c-like enums and casts

--- src/03-enums.rs
+++ src/03-enums.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 
 enum WebEvent {
     // everyone of those is different and independent from one another.
@@ -39,6 +40,60 @@ impl VeryVerboseEnumDoToThingsWithNumbers {
     }
 }
 
+enum Status {
+    Rich,
+    MiddleClass,
+    Poor
+}
+
+enum Work {
+    Civilian,
+    Soldier
+}
+
+fn use_for_enums() {
+
+    // We could use these enum constants in this scope without explicit declaration
+    use Status::{Rich, Poor};
+    let status = Poor;
+    // Include everything
+    use Work::*;
+    let work = Civilian;
+
+    match status {
+        Rich => println!("The rich have lots of money!"),
+        Status::MiddleClass => println!("Those ones earn not less than 17k per month!"),
+        Poor => println!("The poor have no money...")
+    }
+
+    match work {
+        Civilian => println!("Civilians work!"),
+        Soldier => println!("Soldiers fight!")
+    }
+}
+
+// C-like enum with implicit discriminator (starts at 0)
+enum Number {
+    Zero,
+    One,
+    Two
+}
+
+// C-like enum with explicit discriminators
+enum Color {
+    Red = 0xff0000,
+    Green = 0x00ff00,
+    Blue = 0x0000ff
+}
+
+fn cast_enums() {
+    println!("Zero is {}", Number::Zero as i32);
+    println!("One is {}", Number::One as i32);
+
+    println!("roses are #{:06x}", Color::Red as i32);
+    println!("violets are #{:06x}", Color::Blue as i32);
+}
+
 fn main() {
     let pressed = WebEvent::KeyPress('x');
     // `to_owned()` creates an owned `String` from a string slice.
@@ -56,4 +111,8 @@ fn main() {
     let t = Operations::Add;
     let result = t.fun(15, 17);
     println!("Result of Operations::Add is {}", result);
+
+    use_for_enums();
+
+    cast_enums();
 }
